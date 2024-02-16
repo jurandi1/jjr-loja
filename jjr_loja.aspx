ffc17f0ece55efe7407c18e46e0cd595ef8fd774
Jurandi Junior <jurandijr20@gmail.com>
2024-02-16 19:01:00 -0300

    Botão de pesquisa configurado

--- jjr_loja.aspx
+++ jjr_loja.aspx
@@ -26,6 +26,7 @@
             <a class="cabecalho__menu__link" href="prod_fem.aspx">Mulher</a>
             <a class="cabecalho__menu__link" href="unisex.aspx">Unisex</a>
             <form class="cabecalho__menu__search" runat="server">
+                <asp:TextBox ID="searchBox" runat="server" placeHolder="'Homem', 'Mulher', 'Unisex'"/>
                 <asp:LinkButton ID="btnPesquisar" runat="server" OnClick="btnPesquisar_Click">
                         <img src="img/site/search.jpg" style="width: 35px; height: 35px;">
                 </asp:LinkButton>
--- jjr_loja.aspx.cs
+++ jjr_loja.aspx.cs
@@ -1,4 +1,6 @@
 ﻿using System;
+using System.Collections.Generic;
+using System.Linq;
 
 public partial class jjr_loja : System.Web.UI.Page
 {
@@ -6,13 +8,71 @@ public partial class jjr_loja : System.Web.UI.Page
     {
         if (!IsPostBack)
         {
-            
+
         }
     }
 
+    private static readonly Dictionary<string, string> KeywordToPage = new Dictionary<string, string>(StringComparer.OrdinalIgnoreCase)
+    {
+        // Masculinas
+        { "Homem", "prod_masc.aspx" },
+        { "Camiseta masculina", "prod_masc.aspx" },
+        { "Cuecas", "prod_masc.aspx" },
+        { "Roupa masculina", "prod_masc.aspx" },
+        { "Sapato masculino", "prod_masc.aspx" },
+        { "Acessórios masculinos", "prod_masc.aspx" },
+        { "Blusa masculina", "prod_masc.aspx" },
+        { "Jaqueta masculina", "prod_masc.aspx" },
+        { "Calça masculina", "prod_masc.aspx" },
+        { "Short masculino", "prod_masc.aspx" },
+        { "Meia masculina", "prod_masc.aspx" },
+        { "Gravata", "prod_masc.aspx" },
+        { "Boné", "prod_masc.aspx" },
+
+        // Femininas
+        { "Calcinha", "prod_fem.aspx" },
+        { "Feminina", "prod_fem.aspx" },
+        { "Mulher", "prod_fem.aspx" },
+        { "Roupa feminina", "prod_fem.aspx" },
+        { "Sapato feminino", "prod_fem.aspx" },
+        { "Acessórios femininos", "prod_fem.aspx" },
+        { "Blusa feminina", "prod_fem.aspx" },
+        { "Vestido", "prod_fem.aspx" },
+        { "Saia", "prod_fem.aspx" },
+        { "Calça feminina", "prod_fem.aspx" },
+        { "Short feminino", "prod_fem.aspx" },
+        { "Meia feminina", "prod_fem.aspx" },
+        { "Bolsa", "prod_fem.aspx" },
+
+        // Unisex
+        { "Unisex", "unisex.aspx" },
+        { "Meias", "unisex.aspx" },
+        { "Camiseta", "unisex.aspx" },
+        { "Calça jeans", "unisex.aspx" },
+        { "Tênis", "unisex.aspx" },
+        { "Mochila", "unisex.aspx" },
+        { "Relógio", "unisex.aspx" },
+        { "Óculos", "unisex.aspx" },
+        { "Chapéu", "unisex.aspx" },
+        { "Gorro", "unisex.aspx" },
+        { "Cinto", "unisex.aspx" },
+        { "Luvas", "unisex.aspx" },
+        { "Cachecol", "unisex.aspx" },
+    };
 
     protected void btnPesquisar_Click(object sender, EventArgs e)
     {
-        Response.Redirect("jjr_loja.aspx");
+        string query = searchBox.Text;
+        string page = KeywordToPage.FirstOrDefault(k => query.Contains(k.Key)).Value;
+
+        if (!string.IsNullOrEmpty(page))
+        {
+            Response.Redirect(page);
+        }
+        else
+        {
+            Response.Redirect("jjr_loja.aspx?q=" + Server.UrlEncode(query));
+        }
     }
+
 }
